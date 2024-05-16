3f193366f1ba23cd0134b4bbb2d5f6f83fae1cb4
Gabriel Lebis - Gableb <gableb@hotmail.fr>
2024-05-16 14:54:42 +0000

    Add CardSvgMap

---
+++ gabo-client/src/lib/assets/CardSvgMap.ts
@@ -0,0 +1,112 @@
+//TODO export object containng all svgs to be used with a parameter
+import svg2S from "$lib/assets/cards/2S.svg";
+import svg3S from "$lib/assets/cards/3S.svg";
+import svg4S from "$lib/assets/cards/4S.svg";
+import svg5S from "$lib/assets/cards/5S.svg";
+import svg6S from "$lib/assets/cards/6S.svg";
+import svg7S from "$lib/assets/cards/7S.svg";
+import svg8S from "$lib/assets/cards/8S.svg";
+import svg9S from "$lib/assets/cards/9S.svg";
+import svg10S from "$lib/assets/cards/10S.svg";
+import svgJS from "$lib/assets/cards/JS.svg";
+import svgQS from "$lib/assets/cards/QS.svg";
+import svgKS from "$lib/assets/cards/KS.svg";
+import svgAS from "$lib/assets/cards/AS.svg";
+import svg2D from "$lib/assets/cards/2D.svg";
+import svg3D from "$lib/assets/cards/3D.svg";
+import svg4D from "$lib/assets/cards/4D.svg";
+import svg5D from "$lib/assets/cards/5D.svg";
+import svg6D from "$lib/assets/cards/6D.svg";
+import svg7D from "$lib/assets/cards/7D.svg";
+import svg8D from "$lib/assets/cards/8D.svg";
+import svg9D from "$lib/assets/cards/9D.svg";
+import svg10D from "$lib/assets/cards/10D.svg";
+import svgJD from "$lib/assets/cards/JD.svg";
+import svgQD from "$lib/assets/cards/QD.svg";
+import svgKD from "$lib/assets/cards/KD.svg";
+import svgAD from "$lib/assets/cards/AD.svg";
+import svg2C from "$lib/assets/cards/2C.svg";
+import svg3C from "$lib/assets/cards/3C.svg";
+import svg4C from "$lib/assets/cards/4C.svg";
+import svg5C from "$lib/assets/cards/5C.svg";
+import svg6C from "$lib/assets/cards/6C.svg";
+import svg7C from "$lib/assets/cards/7C.svg";
+import svg8C from "$lib/assets/cards/8C.svg";
+import svg9C from "$lib/assets/cards/9C.svg";
+import svg10C from "$lib/assets/cards/10C.svg";
+import svgJC from "$lib/assets/cards/JC.svg";
+import svgQC from "$lib/assets/cards/QC.svg";
+import svgKC from "$lib/assets/cards/KC.svg";
+import svgAC from "$lib/assets/cards/AC.svg";
+import svg2H from "$lib/assets/cards/2H.svg";
+import svg3H from "$lib/assets/cards/3H.svg";
+import svg4H from "$lib/assets/cards/4H.svg";
+import svg5H from "$lib/assets/cards/5H.svg";
+import svg6H from "$lib/assets/cards/6H.svg";
+import svg7H from "$lib/assets/cards/7H.svg";
+import svg8H from "$lib/assets/cards/8H.svg";
+import svg9H from "$lib/assets/cards/9H.svg";
+import svg10H from "$lib/assets/cards/10H.svg";
+import svgJH from "$lib/assets/cards/JH.svg";
+import svgQH from "$lib/assets/cards/QH.svg";
+import svgKH from "$lib/assets/cards/KH.svg";
+import svgAH from "$lib/assets/cards/AH.svg";
+import svgBLUE_BACK from "$lib/assets/cards/BLUE_BACK.svg";
+import svgRED_BACK from "$lib/assets/cards/RED_BACK.svg";
+
+export const CardSvgMap = {
+    "2S" : svg2S,
+"3S" : svg3S,
+"4S" : svg4S,
+"5S" : svg5S,
+"6S" : svg6S,
+"7S" : svg7S,
+"8S" : svg8S,
+"9S" : svg9S,
+"10S" : svg10S,
+"JS" : svgJS,
+"QS" : svgQS,
+"KS" : svgKS,
+"AS" : svgAS,
+"2D" : svg2D,
+"3D" : svg3D,
+"4D" : svg4D,
+"5D" : svg5D,
+"6D" : svg6D,
+"7D" : svg7D,
+"8D" : svg8D,
+"9D" : svg9D,
+"10D" : svg10D,
+"JD" : svgJD,
+"QD" : svgQD,
+"KD" : svgKD,
+"AD" : svgAD,
+"2C" : svg2C,
+"3C" : svg3C,
+"4C" : svg4C,
+"5C" : svg5C,
+"6C" : svg6C,
+"7C" : svg7C,
+"8C" : svg8C,
+"9C" : svg9C,
+"10C" : svg10C,
+"JC" : svgJC,
+"QC" : svgQC,
+"KC" : svgKC,
+"AC" : svgAC,
+"2H" : svg2H,
+"3H" : svg3H,
+"4H" : svg4H,
+"5H" : svg5H,
+"6H" : svg6H,
+"7H" : svg7H,
+"8H" : svg8H,
+"9H" : svg9H,
+"10H" : svg10H,
+"JH" : svgJH,
+"QH" : svgQH,
+"KH" : svgKH,
+"AH" : svgAH,
+"BLUE_BACK" : svgBLUE_BACK,
+"RED_BACK" : svgRED_BACK,
+};
