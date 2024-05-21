62b855154849c1eaa530b810ea43c22043af2ba7
Gabriel Lebis - Gableb <gableb@hotmail.fr>
2024-05-21 13:15:33 +0000

    unify indentation to 2 spaces

--- gabo-client/src/routes/+page.svelte
+++ gabo-client/src/routes/+page.svelte
@@ -1,104 +1,104 @@
 <script lang="ts">
-	import { io } from 'socket.io-client';
-	import { ErrorCode } from '@gabo-common/SocketEvents.js';
-	import {
-		type ClientGame,
-		type SerializableClientGame,
-		deserizalizeClientGame
-	} from '@gabo-common/ClientGame';
-	import type { Card } from '@gabo-common/Card';
-	import { CardSvgMap } from '$lib/assets/CardSvgMap';
+  import { io } from 'socket.io-client';
+  import { ErrorCode } from '@gabo-common/SocketEvents.js';
+  import {
+    type ClientGame,
+    type SerializableClientGame,
+    deserizalizeClientGame
+  } from '@gabo-common/ClientGame';
+  import type { Card } from '@gabo-common/Card';
+  import { CardSvgMap } from '$lib/assets/CardSvgMap';
 
-	export const socket = io('ws://localhost:8080');
-	export let cards: Card[] = [{ value: 'KH' }];
-	export let socketID: string | null = null;
-	export let ingame = false;
-	export let nickname = '';
-	export let roomcode = '';
-	export let game: ClientGame | null = null;
+  export const socket = io('ws://localhost:8080');
+  export let cards: Card[] = [{ value: 'KH' }];
+  export let socketID: string | null = null;
+  export let ingame = false;
+  export let nickname = '';
+  export let roomcode = '';
+  export let game: ClientGame | null = null;
 
-	function joinGame() {
-		console.log(nickname, roomcode);
-		socket.emit(
-			'addPlayer',
-			nickname,
-			roomcode,
-			(result: ErrorCode, message: string, serializedGame: SerializableClientGame | null) => {
-				if (result == ErrorCode.Success) {
-					ingame = true;
-					game = deserizalizeClientGame(serializedGame!);
-					console.log(game);
-				} else {
-					console.error('addPlayer was not successful');
-					console.error(message);
-					ingame = false;
-				}
-			}
-		);
-	}
+  function joinGame() {
+    console.log(nickname, roomcode);
+    socket.emit(
+      'addPlayer',
+      nickname,
+      roomcode,
+      (result: ErrorCode, message: string, serializedGame: SerializableClientGame | null) => {
+        if (result == ErrorCode.Success) {
+          ingame = true;
+          game = deserizalizeClientGame(serializedGame!);
+          console.log(game);
+        } else {
+          console.error('addPlayer was not successful');
+          console.error(message);
+          ingame = false;
+        }
+      }
+    );
+  }
 
-	socket.on('connect', () => {
-		console.log('connected', socket.id);
-		socketID = socket.id as string;
-	});
-	socket.on('noArg', () => {
-		console.log('noArg', socket.id);
-	});
-	export const sendHello = () => {
-		socket.emit('hello');
-	};
+  socket.on('connect', () => {
+    console.log('connected', socket.id);
+    socketID = socket.id as string;
+  });
+  socket.on('noArg', () => {
+    console.log('noArg', socket.id);
+  });
+  export const sendHello = () => {
+    socket.emit('hello');
+  };
 </script>
 
 <div class="text-center">
-	{#if ingame}
-		<div id="gameboard">
-			<div id="hand">
-				{#each cards as card}
-					<div>{card.value}</div>
-					<img src={CardSvgMap[card.value]} />
-				{/each}
-			</div>
-		</div>
-	{:else}
-		<form id="login-form" class="m-auto px-2" on:submit|preventDefault={joinGame}>
-			<h1 class="h1">Un Gabo ?</h1>
-			<input
-				type="text"
-				name="nickname"
-				class="form-control mb-2"
-				placeholder="Nickname"
-				required
-				bind:value={nickname}
-			/>
-			<input
-				type="text"
-				name="roomcode"
-				class="form-control mb-2"
-				placeholder="Room code"
-				required
-				bind:value={roomcode}
-			/>
-			<input
-				type="submit"
-				name="join-room"
-				value="Join room"
-				class="btn btn-lg btn-primary btn-block"
-			/>
-		</form>
-	{/if}
-	<p class="mt-2 fw-light fs-6">{socketID ?? 'Disconnected'}</p>
+  {#if ingame}
+    <div id="gameboard">
+      <div id="hand">
+        {#each cards as card}
+          <div>{card.value}</div>
+          <img src={CardSvgMap[card.value]} />
+        {/each}
+      </div>
+    </div>
+  {:else}
+    <form id="login-form" class="m-auto px-2" on:submit|preventDefault={joinGame}>
+      <h1 class="h1">Un Gabo ?</h1>
+      <input
+        type="text"
+        name="nickname"
+        class="form-control mb-2"
+        placeholder="Nickname"
+        required
+        bind:value={nickname}
+      />
+      <input
+        type="text"
+        name="roomcode"
+        class="form-control mb-2"
+        placeholder="Room code"
+        required
+        bind:value={roomcode}
+      />
+      <input
+        type="submit"
+        name="join-room"
+        value="Join room"
+        class="btn btn-lg btn-primary btn-block"
+      />
+    </form>
+  {/if}
+  <p class="mt-2 fw-light fs-6">{socketID ?? 'Disconnected'}</p>
 </div>
 
 <button on:click={sendHello}>send hello</button>
 
 <style>
-	#login-form {
-		display: flex;
-		flex-direction: column;
-	}
-	@media (min-width: 768px) {
-		#login-form {
-			max-width: 30%;
-		}
-	}
+  #login-form {
+    display: flex;
+    flex-direction: column;
+  }
+  @media (min-width: 768px) {
+    #login-form {
+      max-width: 30%;
+    }
+  }
 </style>
